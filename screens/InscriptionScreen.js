dcc1e867f2b7a625c1d651dd3384281eac728550
XerOce <louis.huguet@epitech.eu>
2023-06-06 21:07:45 +0200

    ADD: google-services.json

--- screens/InscriptionScreen.js
+++ screens/InscriptionScreen.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import { StyleSheet, Text, View } from 'react-native';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDISP6YR0HDz433kYvqOyGJ0YT13cjPWzI',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'my-snapchat-ac679',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const InscriptionScreen = () => {
   return (
